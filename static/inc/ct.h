e191d4225cb0410dc555503e3d2825aa56dd8324
sobriodev <sobriodev@gmail.com>
2017-12-23 16:34:44 +0100

    Timer reset function

--- static/inc/ct.h
+++ static/inc/ct.h
@@ -8,6 +8,19 @@
 #ifndef CT_H_
 #define CT_H_
 
+#include "chip.h"
+
+#define TIMER_ENABLE_POS 0
+#define TIMER_ENABLE_MASK _BIT(TIMER_ENABLE_POS)
+#define TIMER_RESET_POS 1
+#define TIMER_RESET_MASK _BIT(TIMER_RESET_POS)
+
+/**
+ * @brief 	Resets the particular timer
+ * @param 	timer	:  Pointer to timer
+ * @return 	Nothing
+ */
+void Timer_Reset(LPC_TIMER_T *timer);
 
 
 #endif /* CT_H_ */
--- static/src/ct.c
+++ static/src/ct.c
@@ -6,3 +6,11 @@
  */
 
 
+#include "../inc/ct.h"
+
+void Timer_Reset(LPC_TIMER_T *timer) {
+	timer->TC = 1;
+	timer->TCR |= TIMER_RESET_MASK;
+	while (timer->TC != 0) {}
+	timer->TCR &= ~(TIMER_RESET_MASK);
+}
