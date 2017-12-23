ec1fff1ba2cdfbe31b84467ac57589894a375f30
sobriodev <sobriodev@gmail.com>
2017-12-23 16:47:48 +0100

    Fixed comments

--- static/inc/ct.h
+++ static/inc/ct.h
@@ -10,9 +10,9 @@
 
 #include "chip.h"
 
-#define TIMER_ENABLE_POS 	0                       /* TCR : CEN position */
+#define TIMER_ENABLE_POS 	0						/* TCR : CEN position */
 #define TIMER_ENABLE_MASK	_BIT(TIMER_ENABLE_POS)	/* TCR : CEN mask */
-#define TIMER_RESET_POS 	1                       /* TCR : CRST position */
+#define TIMER_RESET_POS 	1						/* TCR : CRST position */
 #define TIMER_RESET_MASK 	_BIT(TIMER_RESET_POS)	/* TCR : CRST mask */
 
 /**
