75ec6845eef6e9aa99bdf2ed0906bc9b0a790536
sobriodev <sobriodev@gmail.com>
2017-12-23 16:56:05 +0100

    Fixed tabs and wrong comments display

--- static/inc/ct.h
+++ static/inc/ct.h
@@ -10,14 +10,14 @@
 
 #include "chip.h"
 
-#define TIMER_ENABLE_POS 	0						/* TCR : CEN position */
-#define TIMER_ENABLE_MASK	_BIT(TIMER_ENABLE_POS)	/* TCR : CEN mask */
-#define TIMER_RESET_POS 	1						/* TCR : CRST position */
-#define TIMER_RESET_MASK 	_BIT(TIMER_RESET_POS)	/* TCR : CRST mask */
+#define TIMER_ENABLE_POS    0                       /* TCR : CEN position */
+#define TIMER_ENABLE_MASK   _BIT(TIMER_ENABLE_POS)  /* TCR : CEN mask */
+#define TIMER_RESET_POS     1                       /* TCR : CRST position */
+#define TIMER_RESET_MASK    _BIT(TIMER_RESET_POS)   /* TCR : CRST mask */
 
 /**
  * @brief 	Resets the particular timer
- * @param 	timer	:  Pointer to timer
+ * @param 	timer : Pointer to timer
  * @return 	Nothing
  */
 void Timer_Reset(LPC_TIMER_T *timer);
