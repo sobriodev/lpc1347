369707e6d9bfb46d98da4b7e3858322c96bf66b9
sobriodev <sobriodev@gmail.com>
2017-12-24 22:21:16 +0100

    Initial CT16B0 interrupt application commit

--- ct/ct16b_int/src/ct16b_int.c
+++ ct/ct16b_int/src/ct16b_int.c
@@ -1,48 +1,89 @@
 /*
 ===============================================================================
- Name        : ct16b_int.c
- Author      : $(author)
- Version     :
- Copyright   : $(copyright)
- Description : main definition
+ Author      : sobriodev
+ Description : Toggle pin's states using interrupt channels
 ===============================================================================
 */
 
-#if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
-#include "chip.h"
-#else
 #include "board.h"
-#endif
-#endif
-
+#include "../../static/inc/ct.h"
 #include <cr_section_macros.h>
 
-// TODO: insert other include files here
+#define TIMER           LPC_TIMER16_0
+#define GPIO            LPC_GPIO_PORT
+#define IOCON           LPC_IOCON
+#define SYSCTL          LPC_SYSCTL
 
-// TODO: insert other definitions and declarations here
+#define PRESCALE_VAL    36000           /* f = 72MHz, output T = 0.5ms => output f = 2kHz, 72MHz / 2kHZ = 36000 */
 
-int main(void) {
+#define MATCH0          0               /* Match channel 0 */
+#define MATCH1          1               /* Match channel 1 */
+#define MATCH2          2               /* Match channel 2 */
+#define MATCH3          3               /* Match channel 3 */
 
-#if defined (__USE_LPCOPEN)
-    // Read clock settings and update SystemCoreClock variable
-    SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
+#define MATCH0_VAL      500             /* Interrupt 0 after 0.25s */
+#define MATCH1_VAL      1000            /* Interrupt 1 after 0.5s */
+#define MATCH2_VAL      2000            /* Interrupt 2 after 1s */
+#define MATCH3_VAL      4000            /* Interrupt 3 after 2s */
 
-    // TODO: insert code here
+#define GPIO_PORT       1               /* Output port */
+#define GPIO_START_PIN  19              /* Output start pin */
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
+/**
+ * @brief   Timer interrupt handler
+ * @return  Nothing
+ * Check if particular match channel interrupt is pending. If yes, set corresponding output pin high, or set all pins low when MATCH3 interrupt flag is set.
+ */
+void TIMER16_0_IRQHandler(void) {
+    if (TIMER->IR & TIMER_MATCH_INT(MATCH0)) {
+        TIMER->IR = TIMER_MATCH_INT(MATCH0);
+        GPIO->B[GPIO_PORT][GPIO_START_PIN] = 1;
+    } else if (TIMER->IR & TIMER_MATCH_INT(MATCH1)) {
+        TIMER->IR = TIMER_MATCH_INT(MATCH1);
+        GPIO->B[GPIO_PORT][GPIO_START_PIN + 1] = 1;
+    } else if (TIMER->IR & TIMER_MATCH_INT(MATCH2)) {
+        TIMER->IR = TIMER_MATCH_INT(MATCH2);
+        GPIO->B[GPIO_PORT][GPIO_START_PIN + 2] = 1;
+    } else if (TIMER->IR & TIMER_MATCH_INT(MATCH3)) {
+        TIMER->IR = TIMER_MATCH_INT(MATCH3);
+        GPIO->PIN[GPIO_PORT] &= ~(_BIT(GPIO_START_PIN) | _BIT(GPIO_START_PIN + 1) | _BIT(GPIO_START_PIN + 2));
     }
+}
+
+/**
+ * @brief   Main program routine
+ * @return  Function should not return
+ */
+int main(void) {
+    // Generic initialization
+    SystemCoreClockUpdate();
+    Board_Init();
+
+    volatile static uint8_t i;
+    for (i = 0; i < 4; ++i) {
+        IOCON->PIO1[GPIO_START_PIN + i] = IOCON_FUNC0;
+        GPIO->DIR[GPIO_PORT] |= _BIT(GPIO_START_PIN + i);
+        GPIO->B[GPIO_PORT][GPIO_START_PIN + i] = 0;
+    }
+
+    SYSCTL->SYSAHBCLKCTRL |= CT16B0_CLOCK_MASK;
+
+    Timer_Reset(TIMER);
+
+    TIMER->MCR = TIMER_INT_ON_MATCH(MATCH0) | TIMER_INT_ON_MATCH(MATCH1) | TIMER_INT_ON_MATCH(MATCH2) | TIMER_INT_ON_MATCH(MATCH3) | TIMER_RESET_ON_MATCH(MATCH3);
+
+    TIMER->MR[MATCH0] = MATCH0_VAL;
+    TIMER->MR[MATCH1] = MATCH1_VAL;
+    TIMER->MR[MATCH2] = MATCH2_VAL;
+    TIMER->MR[MATCH3] = MATCH3_VAL;
+
+    TIMER->PR = PRESCALE_VAL - 1;
+
+    TIMER->TCR |= TIMER_ENABLE_MASK;
+
+    NVIC->ICPR[0] = NVIC_CT16B0_MASK;
+    NVIC->ISER[0] = NVIC_CT16B0_MASK;
+
+    while(1) {}
     return 0 ;
 }
--- static/inc/ct.h
+++ static/inc/ct.h
@@ -10,10 +10,18 @@
 
 #include "chip.h"
 
-#define TIMER_ENABLE_POS    0                       /* TCR : CEN position */
-#define TIMER_ENABLE_MASK   _BIT(TIMER_ENABLE_POS)  /* TCR : CEN mask */
-#define TIMER_RESET_POS     1                       /* TCR : CRST position */
-#define TIMER_RESET_MASK    _BIT(TIMER_RESET_POS)   /* TCR : CRST mask */
+#define TIMER_ENABLE_MASK   _BIT(0)     /* TCR : CEN Counter enable */
+#define TIMER_RESET_MASK    _BIT(1)     /* TCR : CRST Counter reset */
+
+#define CT16B0_CLOCK_MASK   _BIT(7)     /* SYSAHBCLKCTRL : CT16B0 Clock enable for CT16B0 */
+#define CT16B1_CLOCK_MASK   _BIT(8)     /* SYSAHBCLKCTRL : CT16B1 Clock enable for CT16B1 */
+#define CT32B0_CLOCK_MASK   _BIT(9)     /* SYSAHBCLKCTRL : CT32B0 Clock enable for CT32B0 */
+#define CT32B1_CLOCK_MASK   _BIT(10)    /* SYSAHBCLKCTRL : CT32B1 Clock enable for CT32B1 */
+
+#define NVIC_CT16B0_MASK    _BIT(16)    /* NVIC registers CT16B0 mask */
+#define NVIC_CT16B1_MASK    _BIT(17)    /* NVIC registers CT16B1 mask */
+#define NVIC_CT16B2_MASK    _BIT(18)    /* NVIC registers CT16B2 mask */
+#define NVIC_CT16B3_MASK    _BIT(19)    /* NVIC registers CT16B3 mask */
 
 /**
  * @brief   Resets the particular timer
